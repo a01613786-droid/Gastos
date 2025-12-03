9e46032ca4d8e0464d6413e9a50b9150befab36a
a01613786-droid <a01613786@tec.mx>
2025-12-02 23:12:49 -0600

    Add files via upload

---
+++ Gastos (5).py
@@ -0,0 +1,47 @@
+import numpy as np
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.tree import DecisionTreeRegressor
+
+
+st.write(''' # Predicción de gasto de Joaquín Amaro ''')
+st.image("gastos.jpg", caption="Cuánto gasta Joaquín en una compra según ciertos datos")
+
+st.header('Datos del gasto')
+
+def user_input_features():
+  # Entrada
+  Presupuesto = st.number_input('Presupuesto:', min_value=0, max_value=10000, value = 0, step = 1)
+  tiempo = st.number_input('Tiempo invertido (minutos)', min_value=1, max_value=300, value = 1, step = 1)
+  tipo = st.number_input('Tipo',min_value=1, max_value=6, value = 1, step = 1)
+  momento = st.number_input('Momento:', min_value=1, max_value=3, value = 1, step = 1)
+  num_de_personas = st.number_input('No. de personas', min_value=1, max_value=50, value = 1, step = 1)
+
+  user_input_data = {'Presupuesto': Presupuesto,
+                     'Tiempo invertido (minutos)': tiempo,
+                     'Tipo': tipo,
+                     'Momento': momento,
+                     'No. de personas': num_de_personas}
+
+  features = pd.DataFrame(user_input_data, index=[0])
+
+  return features
+
+df = user_input_features()
+
+gasto =  pd.read_csv('Gastos2.csv', encoding='latin-1')
+X = gasto.drop(columns=['Costo'], errors = 'ignore')
+Y = gasto['Costo']
+
+df = df[X.columns]
+
+classifier = DecisionTreeRegressor(max_depth=5, min_samples_leaf=10, max_features=5, random_state=1613786)
+classifier.fit(X, Y)
+
+prediction = classifier.predict(df)
+
+st.subheader('Predicción de Costo')
+predicted_cost = prediction[0]
+
+st.write(f'El costo de la compra es de: ${predicted_cost}')
